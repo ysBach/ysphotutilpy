c6f361f1eae8a4d1b1d2719a2640b2c95b21c8a7
ysBach <dbstn95@gmail.com>
2019-05-06 16:32:19 +0900

    remove wrong comments and add pandas and astroquery dependency

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 """
-Convenience utilities made by ysBach especially for dealing FITS files in astronomical sciences.
 """
 
 from setuptools import setup, find_packages
 
 setup_requires = []
 install_requires = ['numpy',
+                    'pandas',
                     'astropy >= 2.0',
                     'ccdproc >= 1.3',
-                    'matplotlib >= 2']
+                    'matplotlib >= 2',
+                    'astroquery >= 0.3.9']
 
 classifiers = ["Intended Audience :: Science/Research",
                "Operating System :: OS Independent",
