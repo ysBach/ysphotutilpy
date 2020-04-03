fb5dcf202f730957d89c99fce08ffe6a728c05a9
ysBach <dbstn95@gmail.com>
2020-04-03 10:20:16 +0900

    aperture photometry to add aperture area and return pandas

--- ysphotutilpy/apphot.py
+++ ysphotutilpy/apphot.py
@@ -3,7 +3,7 @@ from warnings import warn
 import numpy as np
 from astropy import units as u
 from astropy.nddata import CCDData
-from astropy.table import hstack
+from astropy.table import hstack, QTable
 from photutils import aperture_photometry
 
 from .background import sky_fit
@@ -15,7 +15,8 @@ __all__ = ["apphot_annulus"]
 # TODO: use variance instead of error (see photutils 0.7)
 def apphot_annulus(ccd, aperture, annulus, t_exposure=None,
                    exposure_key="EXPTIME", error=None, mask=None,
-                   sky_keys={}, t_exposure_unit=u.s, verbose=False,
+                   sky_keys={}, aparea_exact=False,
+                   t_exposure_unit=u.s, verbose=False,
                    pandas=False, **kwargs):
     ''' Do aperture photometry using annulus.
     Parameters
@@ -35,6 +36,10 @@ def apphot_annulus(ccd, aperture, annulus, t_exposure=None,
         kwargs of ``sky_fit``. Mostly one doesn't change the default
         setting, so I intentionally made it to be dict rather than usual
         kwargs, etc.
+    aparea_exact : bool, optional
+        Whether to calculate the aperture area (``'aparea'`` column)
+        exactly or not. If ``True``, the area outside the image  **and**
+        those specified by mask are not counted. Default is ``False``.
     pandas : bool, optional.
         Whether to convert to ``pandas.DataFrame``.
     **kwargs:
@@ -75,47 +80,79 @@ def apphot_annulus(ccd, aperture, annulus, t_exposure=None,
         else:
             _ccd.mask = mask
 
-    skys = sky_fit(_ccd, annulus, **sky_keys)
-    try:
-        n_ap = aperture.area
-    except TypeError:  # prior to photutils 0.7
-        n_ap = aperture.area()
-    # except AttributeError:  # if array of aperture given
-    #     try:
-    #         n_ap = np.array([ap.area for ap in aperture])
-    #     except TypeError:  # prior to photutils 0.7
-    #         n_ap = np.array([ap.area() for ap in aperture])
+    if aparea_exact:
+        _ones = np.ones_like(_ccd.data)
+        _area = aperture_photometry(_ones, aperture, mask=_ccd.mask, **kwargs)
+        n_ap = []
+        for c in _area.colnames:
+            if c.startswith("aperture_sum"):
+                n_ap.append(_area[c][0])
+        n_ap = np.array(n_ap)
+    else:
+        try:
+            n_ap = aperture.area
+        except TypeError:  # prior to photutils 0.7
+            n_ap = aperture.area()
+        except AttributeError:  # if array of aperture given
+            try:
+                n_ap = np.array([ap.area for ap in aperture])
+            except TypeError:  # prior to photutils 0.7
+                n_ap = np.array([ap.area() for ap in aperture])
 
-    phot = aperture_photometry(_ccd.data, aperture,
-                               mask=_ccd.mask, error=err, **kwargs)
+    skys = sky_fit(_ccd, annulus, **sky_keys)
+
+    aperture = np.atleast_1d(aperture)
+    _phot = aperture_photometry(_ccd.data, aperture,
+                                mask=_ccd.mask, error=err, **kwargs)
     # If we use ``_ccd``, photutils deal with the unit, and the lines below
     # will give a lot of headache for units. It's not easy since aperture
     # can be pixel units or angular units (Sky apertures).
     # ysBach 2018-07-26
 
-    phot_f = hstack([phot, skys])
+    # Convert aperture_sum_xx columns into 1-column...
+    n = len(aperture)
+    apsums = []
+    aperrs = []
+    phot = QTable(meta=_phot.meta)
 
-    phot_f['aparea'] = n_ap
-    phot_f["source_sum"] = phot_f["aperture_sum"] - n_ap * phot_f["msky"]
+    for i, c in enumerate(_phot.colnames):
+        if not c.startswith("aperture"):
+            phot[c] = [_phot[c][0]]*n
+        elif c.startswith("aperture_sum_err"):
+            aperrs.append(_phot[c][0])
+        else:
+            apsums.append(_phot[c][0])
+
+    phot["aperture_sum"] = apsums
+    if aperrs:
+        phot["aperture_sum_err"] = aperrs
+    else:
+        phot = _phot
+
+    for c in skys.colnames:
+        phot[c] = [skys[c][0]]*n
+
+    phot['aparea'] = n_ap
+    phot["source_sum"] = phot["aperture_sum"] - n_ap * phot["msky"]
 
     # see, e.g., http://stsdas.stsci.edu/cgi-bin/gethelp.cgi?radprof.hlp
     # Poisson + RDnoise (Poisson includes signal + sky + dark) :
-    var_errmap = phot_f["aperture_sum_err"]**2
+    var_errmap = phot["aperture_sum_err"]**2
     # Sum of n_ap Gaussians (kind of random walk):
-    var_skyrand = n_ap * phot_f["ssky"]**2
+    var_skyrand = n_ap * phot["ssky"]**2
     # The CLT error (although not correct, let me denote it as
     # "systematic" error for simplicity) of the mean estimation is
     # ssky/sqrt(nsky), and that is propagated for n_ap pixels, so we
     # have std = n_ap*ssky/sqrt(nsky), so variance is:
-    var_sky = (n_ap * phot_f['ssky'])**2 / phot_f['nsky']
+    var_sky = (n_ap * phot['ssky'])**2 / phot['nsky']
 
-    phot_f["source_sum_err"] = np.sqrt(var_errmap + var_skyrand + var_sky)
+    phot["source_sum_err"] = np.sqrt(var_errmap + var_skyrand + var_sky)
 
-    phot_f["mag"] = -2.5 * np.log10(phot_f['source_sum'] / t_exposure)
-    phot_f["merr"] = (2.5 / np.log(10)
-                      * phot_f["source_sum_err"] / phot_f['source_sum'])
+    phot["mag"] = -2.5 * np.log10(phot['source_sum'] / t_exposure)
+    phot["merr"] = (2.5 / np.log(10)
+                    * phot["source_sum_err"] / phot['source_sum'])
 
     if pandas:
-        return phot_f.to_pandas()
+        return phot.to_pandas()
     else:
-        return phot_f
+        return phot
